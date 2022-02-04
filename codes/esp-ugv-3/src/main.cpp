d6bde7aa5ea8cd78be548f18eb161baf9062fe6a
tnfssc <tnfssc@gmail.com>
2022-02-04 20:09:59 +0530

    Working React App

--- codes/esp-ugv-3/src/main.cpp
+++ codes/esp-ugv-3/src/main.cpp
@@ -1,34 +1,53 @@
+
 #include <Arduino.h>
 #include <SPIFFS.h>
+#ifdef ESP32
 #include <WiFi.h>
-#include <esp32PWMUtilities.h>
+#include <AsyncTCP.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
 #include <ESPAsyncWebServer.h>
-
-const char *ssid = "beingAlien";
-const char *password = "1234qwer!@#$QWER";
+#include <bits/stdc++.h>
 
 AsyncWebServer server(80);
 
+const char *ssid = "nitro5";
+const char *password = "12345678";
+
+const char *PARAM_MESSAGE = "message";
+
+void notFound(AsyncWebServerRequest *request)
+{
+  request->send(404, "text/plain", "Not found");
+}
+
 void setup()
 {
-  Serial.begin(115200);
+
+  Serial.begin(9600);
   if (!SPIFFS.begin())
   {
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
+  if (WiFi.waitForConnectResult() != WL_CONNECTED)
   {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
+    Serial.println("WiFi failed");
+    return;
   }
+
+  Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(SPIFFS, "/webui/build/index.html", "text/html"); });
-  server.on("/static/js/main.js", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(SPIFFS, "/webui/build/static/js/main.js", "text/javascript"); });
+            { request->send(SPIFFS, "/webui/index.html", "text/html"); });
+
+  server.onNotFound(notFound);
+
   server.begin();
 }
 
