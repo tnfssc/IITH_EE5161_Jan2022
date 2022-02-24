7086b9fc955eb55aa98260da898b763b1d1cc750
tnfssc <tnfssc@gmail.com>
2022-02-24 12:07:10 +0530

    Fix cors

--- codes/esp-ugv-4/src/main.cpp
+++ codes/esp-ugv-4/src/main.cpp
@@ -1,22 +1,14 @@
 
 #include <Arduino.h>
-#include <ArduinoOTA.h>
-#include <SPIFFS.h>
-#ifdef ESP32
 #include <WiFi.h>
 #include <AsyncTCP.h>
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
-#endif
 #include <ESPAsyncWebServer.h>
 #include <esp32PWMUtilities.h>
-#include <bits/stdc++.h>
 
 AsyncWebServer server(80);
 
-const char *ssid = "nitro5";
-const char *password = "12345678";
+const char *ssid = "beingHuman";
+const char *password = "1234qwer!@#$QWER";
 
 const char *PARAM_MESSAGE = "message";
 
@@ -65,11 +57,6 @@ void setup()
   Motor2.attach(15, 18, 19);
 
   Serial.begin(9600);
-  if (!SPIFFS.begin())
-  {
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
-  }
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
@@ -83,27 +70,23 @@ void setup()
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
-  // UI server
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(SPIFFS, "/webui/index.html", "text/html"); });
-
   // Directions
-  server.on("/forward", HTTP_GET, [](AsyncWebServerRequest *request)
+  server.on("/forward", HTTP_POST, [](AsyncWebServerRequest *request)
             {
                 moveForward();
                 request->send(200, "text/plain", "Done!"); });
 
-  server.on("/backward", HTTP_GET, [](AsyncWebServerRequest *request)
+  server.on("/backward", HTTP_POST, [](AsyncWebServerRequest *request)
             {
                 moveBackward();
                 request->send(200, "text/plain", "Done!"); });
 
-  server.on("/left", HTTP_GET, [](AsyncWebServerRequest *request)
+  server.on("/left", HTTP_POST, [](AsyncWebServerRequest *request)
             {
                 moveLeft();
                 request->send(200, "text/plain", "Done!"); });
 
-  server.on("/right", HTTP_GET, [](AsyncWebServerRequest *request)
+  server.on("/right", HTTP_POST, [](AsyncWebServerRequest *request)
             {
                 moveRight();
                 request->send(200, "text/plain", "Done!"); });
@@ -111,38 +94,8 @@ void setup()
   server.onNotFound(notFound);
 
   server.begin();
-
-  ArduinoOTA
-      .onStart([]()
-               {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
-
-      server.end();
-      SPIFFS.end();
-      Serial.println("Start updating " + type); })
-      .onEnd([]()
-             { Serial.println("\nEnd"); })
-      .onProgress([](unsigned int progress, unsigned int total)
-                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
-      .onError([](ota_error_t error)
-               {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed"); })
-      .setPort(3232);
-
-  ArduinoOTA.begin();
 }
 
 void loop()
 {
-  ArduinoOTA.handle();
-  delay(100);
 }
