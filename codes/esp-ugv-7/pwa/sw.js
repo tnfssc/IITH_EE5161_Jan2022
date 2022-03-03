b96a87cec6f382d3c32f2ddcfda28af86c54bdd2
tnfssc <tnfssc@gmail.com>
2022-03-03 09:46:02 +0530

    Added offline support

--- codes/esp-ugv-7/pwa/sw.js
+++ codes/esp-ugv-7/pwa/sw.js
@@ -32,3 +32,11 @@ self.addEventListener("activate", event => {
     })
   );
 });
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
